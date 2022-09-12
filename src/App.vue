6f436bbec05f23012091994f7c88f26c2dbe1460
btea <2356281422@qq.com>
2022-09-12 20:40:44 +0800

    feat: add svg animation

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import Earth from './Main.vue'
+import LineAni from './LineAni.vue'
+
+const current = ref('')
+const goto = (v: string) => {
+	current.value = v
+}
+</script>
+<template>
+	<div class="con">
+		<div class="main" v-if="!current">
+			<div class="earth" @click="goto('earth')">地球</div>
+			<div class="line" @click="goto('line')">路径动画</div>
+		</div>
+		<Earth v-if="current === 'earth'" />
+		<LineAni v-if="current === 'line'" />
+	</div>
+</template>
+<style scoped>
+.con {
+	height: 100vh;
+	width: 100vw;
+	overflow: hidden;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background: linear-gradient(-45deg, #2edce26e, transparent);
+}
+.main {
+	display: flex;
+}
+.earth,.line {
+	margin: 0 10px;
+	color: rgba(102, 204, 255);
+	opacity: .6;
+	cursor: pointer;
+}
+.earth:hover,.line:hover {
+	opacity: 1;
+}
+</style>
---
+++ src/LineAni.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--eos-icons" width="32" height="32" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+		<path id="vv" fill="currentColor" d="m21.732 11l-4.003-7a1.967 1.967 0 0 0-1.72-1H8.005a2.038 2.038 0 0 0-1.733 1l-4.003 7a1.999 1.999 0 0 0 0 2l4.003 7a2.038 2.038 0 0 0 1.733 1h8.006a1.966 1.966 0 0 0 1.719-1l4.003-7a1.999 1.999 0 0 0 0-2ZM16 19H8l-4-7l4-7h8l4 7Z"></path>
+		<circle r="6" fill="#5F95FF"><animateMotion dur="5s" repeatCount="indefinite"><mpath xlink:href="#vv"></mpath></animateMotion></circle>
+	</svg>
+</template>
+<style scoped>
+.iconify {
+	font-size: 45px;
+}
+</style>
